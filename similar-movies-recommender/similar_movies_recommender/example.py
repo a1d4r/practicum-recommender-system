9276ed8500ed911469c7b64d9757ffd230a50e20
Aidar Garikhanov <a1d4r@yandex.ru>
2024-09-08 22:06:31 +0300

    Add service skeleton

---
+++ similar-movies-recommender/similar_movies_recommender/example.py
@@ -0,0 +1,23 @@
+"""Example of code."""
+
+import pydantic
+
+
+class ExampleModel(pydantic.BaseModel):
+    """Example model."""
+
+    foo: str
+    bar: int
+
+
+def hello(name: str) -> str:
+    """Just a greetings example.
+
+    >>> hello("World")
+    'Hello World!'
+    """
+    return f"Hello {name}!"
+
+
+if __name__ == "__main__":
+    print(hello("World"))  # noqa: T201
